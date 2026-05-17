f9814b7f460153e2c5afb03a126278cf5a83a2ef
Ana Luisa <alcreis2303@gmail.com>
2026-05-17 17:40:49 -0300

    Refactor: titulo da main

--- BibliotecaInclusiva/src/main/java/BibliotecaInclusiva/AtivIndividual/BibliotecaInclusivaApplication.java
+++ BibliotecaInclusiva/src/main/java/BibliotecaInclusiva/AtivIndividual/BibliotecaInclusivaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AtivIndividualApplication {
+public class BibliotecaInclusivaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AtivIndividualApplication.class, args);
+		SpringApplication.run(BibliotecaInclusivaApplication.class, args);
 	}
 
 }
